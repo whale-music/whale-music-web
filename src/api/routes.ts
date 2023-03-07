40bc0ce2ed17799f8bc94a8bcf2923f814c054f7
Mach Enike Sakura <15377054673@163.com>
2023-03-07 15:50:53 +0800

    fix: 修复路由跳转

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,43 +1,38 @@
 // import { http } from "@/utils/http";
 
-// type Result = {
-//   success: boolean;
-//   data: Array<any>;
-// };
-
-// export const getAsyncRoutes = () => {
-//   const permissionRouter = {
-//     path: "/permission",
-//     meta: {
-//       title: "menus.permission",
-//       icon: "lollipop",
-//       rank: 10
-//     },
-//     children: [
-//       {
-//         path: "/permission/page/index",
-//         name: "PermissionPage",
-//         meta: {
-//           title: "menus.permissionPage",
-//           roles: ["admin", "common"]
-//         }
-//       },
-//       {
-//         path: "/permission/button/index",
-//         name: "PermissionButton",
-//         meta: {
-//           title: "menus.permissionButton",
-//           roles: ["admin", "common"],
-//           auths: ["btn_add", "btn_edit", "btn_delete"]
-//         }
-//       }
-//     ]
-//   };
-//   // return http.request<Result>("get", "/getAsyncRoutes");
-//   return new Promise((resolve, reject) => {
-//     resolve({
-//       success: true,
-//       data: [permissionRouter]
-//     });
-//   });
-// };
+export const getAsyncRoutes = () => {
+  const permissionRouter = {
+    path: "/permission",
+    meta: {
+      title: "menus.permission",
+      icon: "lollipop",
+      rank: 10
+    },
+    children: [
+      {
+        path: "/permission/page/index",
+        name: "PermissionPage",
+        meta: {
+          title: "menus.permissionPage",
+          roles: ["admin", "common"]
+        }
+      },
+      {
+        path: "/permission/button/index",
+        name: "PermissionButton",
+        meta: {
+          title: "menus.permissionButton",
+          roles: ["admin", "common"],
+          auths: ["btn_add", "btn_edit", "btn_delete"]
+        }
+      }
+    ]
+  };
+  // return http.request<Result>("get", "/getAsyncRoutes");
+  return new Promise(resolve => {
+    resolve({
+      success: true,
+      data: [permissionRouter]
+    });
+  });
+};
