3355523a2f1c1b7caae499c4627cced05d1c009c
Mach Enike Sakura <15377054673@163.com>
2023-03-18 17:18:57 +0800

    perf: 隐藏主界面显示title标签

--- vite.config.ts
+++ vite.config.ts
@@ -46,7 +46,7 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
       proxy: {
         "^/api/.*": {
           // 这里填写后端地址
-          target: "http://127.0.0.1:3000",
+          target: "http://127.0.0.1:8000",
           changeOrigin: true,
           rewrite: path => path.replace(/^\/api/, "")
         }
