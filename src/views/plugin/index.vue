24859230eb099a3e7a638b8a2efd0cabb8920b51
Mach Enike Sakura <15377054673@163.com>
2023-05-02 23:49:29 +0800

    feat: 优化插件列表UI

--- src/views/plugin/index.vue
+++ src/views/plugin/index.vue
@@ -1,102 +1,3 @@
-<template>
-  <div>
-    <div class="ml-4">
-      <h2>插件</h2>
-      <div class="search">
-        <div class="input-box">
-          <el-input
-            size="large"
-            v-model="pluginSearch"
-            placeholder="Please input"
-            class="plugin-input-with-select"
-          />
-          <el-button type="primary" size="large">搜索</el-button>
-        </div>
-        <el-button
-          type="primary"
-          size="large"
-          class="mr-4"
-          @click="this.showDialogFlag = true"
-          >新建</el-button
-        >
-        <el-dialog v-model="this.showDialogFlag" width="30%" center>
-          <el-form
-            label-position="top"
-            label-width="100px"
-            :model="this.pluginInfo"
-            style="max-width: 460px"
-          >
-            <el-form-item label="插件名">
-              <el-input v-model="this.pluginInfo.pluginName" />
-            </el-form-item>
-            <el-form-item label="作者">
-              <el-input v-model="this.pluginInfo.createName" />
-            </el-form-item>
-            <el-form-item label="插件描述">
-              <el-input v-model="this.pluginInfo.description" />
-            </el-form-item>
-          </el-form>
-          <Wbutton type="primary" @click="creatPlugin">创建</Wbutton>
-        </el-dialog>
-      </div>
-    </div>
-    <el-dialog v-model="deletePlugin" width="30%">
-      <span>确定删除吗？</span>
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button @click="deletePlugin = false">否</el-button>
-          <el-button
-            type="primary"
-            @click="
-              () => {
-                this.deletePlugin = false;
-                this.deletePluginMethod();
-              }
-            "
-          >
-            是
-          </el-button>
-        </span>
-      </template>
-    </el-dialog>
-    <div class="plugin-grid" v-if="this.isPluginListShow">
-      <div
-        class="plugin-show"
-        v-for="(i, index) in pluginList"
-        :key="i.id"
-        @contextmenu="onContextMenu($event, i.id, index)"
-      >
-        <div class="flex justify-between">
-          <h3>{{ i.pluginName }}</h3>
-          <div class="flex items-center">
-            <el-icon
-              :size="25"
-              class="cursor-pointer"
-              style="color: var(--el-bg-color)"
-            >
-              <a @click="toRunPlugin(i.id)">
-                <Icon icon="mingcute:play-fill" />
-              </a>
-            </el-icon>
-            <el-button link type="primary" @click="toEditCode(i)">
-              编辑
-            </el-button>
-          </div>
-        </div>
-        <p class="text-xs font-bold text-neutral-500">{{ i.createName }}</p>
-        <p>
-          {{ i.description }}
-        </p>
-      </div>
-    </div>
-    <div v-else>
-      <el-empty>
-        <el-button type="primary">新建</el-button>
-      </el-empty>
-    </div>
-  </div>
-</template>
-
 <script lang="ts">
 import {
   deletePlugin,
@@ -211,7 +112,111 @@ export default {
   }
 };
 </script>
-
+<template>
+  <div>
+    <div class="ml-4">
+      <h2>插件</h2>
+      <div class="search">
+        <div class="input-box">
+          <el-input
+            size="large"
+            v-model="pluginSearch"
+            placeholder="Please input"
+            class="plugin-input-with-select"
+          />
+          <el-button type="primary" size="large">搜索</el-button>
+        </div>
+        <el-button
+          type="primary"
+          size="large"
+          class="mr-4"
+          @click="this.showDialogFlag = true"
+          >新建</el-button
+        >
+        <el-dialog v-model="this.showDialogFlag" width="30%" center>
+          <el-form
+            label-position="top"
+            label-width="100px"
+            :model="this.pluginInfo"
+            style="max-width: 460px"
+          >
+            <el-form-item label="插件名">
+              <el-input v-model="this.pluginInfo.pluginName" />
+            </el-form-item>
+            <el-form-item label="作者">
+              <el-input v-model="this.pluginInfo.createName" />
+            </el-form-item>
+            <el-form-item label="插件描述">
+              <el-input v-model="this.pluginInfo.description" />
+            </el-form-item>
+          </el-form>
+          <Wbutton type="primary" @click="creatPlugin">创建</Wbutton>
+        </el-dialog>
+      </div>
+    </div>
+    <el-dialog v-model="deletePlugin" width="30%">
+      <span>确定删除吗？</span>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="deletePlugin = false">否</el-button>
+          <el-button
+            type="primary"
+            @click="
+              () => {
+                this.deletePlugin = false;
+                this.deletePluginMethod();
+              }
+            "
+          >
+            是
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
+    <div class="plugin-grid" v-if="this.isPluginListShow">
+      <div
+        class="plugin-show"
+        v-for="(i, index) in pluginList"
+        :key="i.id"
+        @contextmenu="onContextMenu($event, i.id, index)"
+      >
+        <div class="flex justify-between mb-4">
+          <IconifyIconOnline
+            class="cursor-pointer"
+            style="color: var(--el-color-primary)"
+            icon="solar:plug-circle-bold-duotone"
+            width="3rem"
+            height="3rem"
+          />
+          <div class="flex items-center">
+            <el-icon
+              :size="25"
+              class="cursor-pointer"
+              style="color: var(--el-bg-color)"
+            >
+              <a @click="toRunPlugin(i.id)">
+                <Icon icon="mingcute:play-fill" />
+              </a>
+            </el-icon>
+            <el-button link type="primary" @click="toEditCode(i)">
+              编辑
+            </el-button>
+          </div>
+        </div>
+        <h3>{{ i.pluginName }}</h3>
+        <p class="text-xs font-bold text-neutral-500">{{ i.createName }}</p>
+        <p class="h-5 truncate">
+          {{ i.description }}
+        </p>
+      </div>
+    </div>
+    <div v-else>
+      <el-empty>
+        <el-button type="primary">新建</el-button>
+      </el-empty>
+    </div>
+  </div>
+</template>
 <style lang="scss" scoped>
 @import "@/style/element/input.scss";
 
@@ -224,6 +229,7 @@ export default {
 .search {
   display: flex;
   justify-content: space-between;
+  align-items: center;
 }
 
 .plugin-input-with-select {
@@ -240,10 +246,10 @@ export default {
 .plugin-show {
   height: 10rem;
   border-radius: 1rem;
-  border: 1px solid var(--el-color-primary-light-3);
+  border: 1px solid var(--el-border);
   padding: 1rem;
   margin: 1rem;
-  background-color: var(--el-color-primary-light-5);
+  background-color: var(--el-bg-color);
 }
 
 /* 选择框样式，没有生效 */
