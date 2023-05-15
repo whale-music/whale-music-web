d572aa97b0b730c0eb733fa76023f6cf0b7f4f8e
Mach Enike Sakura <15377054673@163.com>
2023-05-16 03:22:02 +0800

    feat: 优化专辑详细信息界面加载骨架Loading

--- src/views/music/albumInfo/index.vue
+++ src/views/music/albumInfo/index.vue
@@ -32,13 +32,16 @@ const albumInfo = ref<AlbumDataRes>({
   updateTime: ""
 });
 
+const skeletonLoadingFlag = ref<boolean>(false);
 onBeforeMount(() => {
   albumId.value = useRouter().currentRoute.value.query.id;
+  skeletonLoadingFlag.value = true;
   console.log(albumId);
   getAlbumDataInfo(albumId.value).then(res => {
     albumInfo.value = res.data;
     modifyAlbumInfo.value = clone(albumInfo.value, true);
     publishTime.value = clone(modifyAlbumInfo.value.publishTime, true);
+    skeletonLoadingFlag.value = false;
   });
 });
 
@@ -132,138 +135,208 @@ const toArtist = id => {
         <el-button @click="saveOrUpdate" type="primary">更新</el-button>
       </template>
     </el-dialog>
-    <div class="flex">
-      <LoadImg :src="albumInfo.pic" />
-      <div class="show-artist-data">
-        <div>
-          <span class="name">{{ albumInfo.albumName }}</span>
-          <br />
-          <div class="flex items-center">
-            <span>艺术家:&#32;</span>
-            <el-link
-              :underline="false"
-              @click="toArtist(item.id)"
-              v-for="(item, index) in albumInfo.artistList"
-              :key="index"
-              ><span class="font-semibold" v-html="item.artistName + '\u00a0'"
-            /></el-link>
+    <el-skeleton :loading="skeletonLoadingFlag">
+      <template #template>
+        <div class="layout-container">
+          <el-skeleton-item
+            variant="image"
+            style="width: 20rem; height: 20rem; border-radius: 2rem"
+          />
+          <div class="flex flex-col gap-2">
+            <el-skeleton-item
+              variant="h1"
+              style="height: 3rem; width: 30rem; border-radius: 1rem"
+            />
+            <div class="flex flex-col gap-1">
+              <el-skeleton-item
+                variant="p"
+                style="height: 1.3rem; width: 10rem"
+              />
+              <el-skeleton-item
+                variant="p"
+                style="height: 1.3rem; width: 7rem"
+              />
+              <el-skeleton-item
+                variant="p"
+                style="height: 1.3rem; width: 9rem"
+              />
+              <el-skeleton />
+              <el-skeleton-item
+                variant="button"
+                style="width: 6rem; height: 2.3rem; border-radius: 2rem"
+              />
+            </div>
           </div>
-          <span>类型: </span>
-          <span>{{ albumInfo.subType }}</span>
-          <br />
-          <div
-            v-if="
-              albumInfo.company !== '' &&
-              albumInfo.company !== null &&
-              albumInfo.company !== undefined
-            "
-          >
-            <span>唱片公司: </span>
-            <span>{{ albumInfo.company }}</span>
-          </div>
-          <div>
-            <span
-              >发布时间:
-              {{ dateFormater("YYYY-MM-dd", albumInfo.publishTime) }}</span
-            >
-          </div>
-          <br />
-          <div>
-            <p class="content">
-              <span class="font-bold">{{ albumInfo.description }} </span>
-            </p>
-            <el-link
-              class="tail"
-              :underline="false"
-              @click="centerDialogVisible = !centerDialogVisible"
-              >[详情]
-            </el-link>
-
-            <!--显示专辑详细信息-->
-            <el-dialog
-              class="showDialog"
-              v-model="centerDialogVisible"
-              width="45%"
-              :show-close="false"
-            >
-              <template #header>
-                <h2>{{ albumInfo.albumName }}</h2>
-                <span
-                  class="text-sm text-neutral-400"
+        </div>
+      </template>
+      <template #default>
+        <div class="layout-container">
+          <LoadImg :src="albumInfo.pic" />
+          <div class="show-artist-data">
+            <div>
+              <span class="name">{{ albumInfo.albumName }}</span>
+              <br />
+              <div class="flex items-center">
+                <span>艺术家:&#32;</span>
+                <el-link
+                  :underline="false"
+                  @click="toArtist(item.id)"
                   v-for="(item, index) in albumInfo.artistList"
                   :key="index"
-                  >{{ item.artistName }}</span
+                  ><span
+                    class="font-semibold"
+                    v-html="item.artistName + '\u00a0'"
+                /></el-link>
+              </div>
+              <span>类型: </span>
+              <span>{{ albumInfo.subType }}</span>
+              <br />
+              <div
+                v-if="
+                  albumInfo.company !== '' &&
+                  albumInfo.company !== null &&
+                  albumInfo.company !== undefined
+                "
+              >
+                <span>唱片公司: </span>
+                <span>{{ albumInfo.company }}</span>
+              </div>
+              <div>
+                <span
+                  >发布时间:
+                  {{ dateFormater("YYYY-MM-dd", albumInfo.publishTime) }}</span
                 >
-                <span class="text-sm text-neutral-400">&#32;·&#32;</span>
-                <span class="text-sm text-neutral-400">
-                  {{ dateFormater("YYYY", albumInfo.publishTime) }}</span
+              </div>
+              <br />
+              <div>
+                <p class="content">
+                  <span class="font-bold">{{ albumInfo.description }} </span>
+                </p>
+                <el-link
+                  class="tail"
+                  :underline="false"
+                  @click="centerDialogVisible = !centerDialogVisible"
+                  >[详情]
+                </el-link>
+
+                <!--显示专辑详细信息-->
+                <el-dialog
+                  class="showDialog"
+                  v-model="centerDialogVisible"
+                  width="45%"
+                  :show-close="false"
                 >
-              </template>
-              <el-scrollbar class="show-desc">
-                <span class="whitespace-pre-wrap">{{
-                  albumInfo.description
-                }}</span>
-              </el-scrollbar>
-            </el-dialog>
+                  <template #header>
+                    <h2>{{ albumInfo.albumName }}</h2>
+                    <span
+                      class="text-sm text-neutral-400"
+                      v-for="(item, index) in albumInfo.artistList"
+                      :key="index"
+                      >{{ item.artistName }}</span
+                    >
+                    <span class="text-sm text-neutral-400">&#32;·&#32;</span>
+                    <span class="text-sm text-neutral-400">
+                      {{ dateFormater("YYYY", albumInfo.publishTime) }}</span
+                    >
+                  </template>
+                  <el-scrollbar class="show-desc">
+                    <span class="whitespace-pre-wrap">{{
+                      albumInfo.description
+                    }}</span>
+                  </el-scrollbar>
+                </el-dialog>
+              </div>
+            </div>
+            <div class="mt-2">
+              <el-button @click="editAlbumInfoFlag = true" type="primary" round
+                >编辑专辑</el-button
+              >
+            </div>
           </div>
         </div>
-        <div class="mt-2">
-          <el-button @click="editAlbumInfoFlag = true" type="primary" round
-            >编辑专辑</el-button
+      </template>
+    </el-skeleton>
+    <!--专辑音乐列表-->
+    <el-skeleton :loading="skeletonLoadingFlag">
+      <template #template>
+        <el-skeleton-item
+          variant="h1"
+          style="
+            margin-top: 1rem;
+            width: 100%;
+            height: 3.6rem;
+            border-radius: 1rem;
+          "
+        />
+      </template>
+      <template #default>
+        <div class="item">
+          <div
+            :class="{
+              'music-item1': index % 2 !== 1,
+              'music-item2': index % 2 !== 0
+            }"
+            :style="{
+              'border-top-left-radius': index === 0 ? '1rem' : '0',
+              'border-top-right-radius': index === 0 ? '1rem' : '0',
+              'border-bottom-left-radius':
+                index === albumInfo.musicList.length - 1 ? '1rem' : '0',
+              'border-bottom-right-radius':
+                index === albumInfo.musicList.length - 1 ? '1rem' : '0'
+            }"
+            v-for="(item, index) in albumInfo.musicList"
+            :key="index"
           >
+            <div
+              @click="toMusicInfo(item.id)"
+              class="h-full flex items-center cursor-pointer"
+            >
+              <span class="music index">{{ index + 1 }}</span>
+              <span class="music">
+                {{ item.musicName }}
+              </span>
+            </div>
+            <div
+              class="h-full grow cursor-pointer"
+              @click="toMusicInfo(item.id)"
+            />
+            <div>
+              <span class="mr-4">{{
+                dateFormater("mm:ss", item.timeLength)
+              }}</span>
+              <el-link :underline="false" class="mr-8">
+                <el-dropdown>
+                  <el-icon :size="20" class="cursor-pointer">
+                    <Icon icon="ep:more-filled" />
+                  </el-icon>
+                  <template #dropdown>
+                    <el-dropdown-menu>
+                      <el-dropdown-item>Action 1</el-dropdown-item>
+                      <el-dropdown-item>Action 2</el-dropdown-item>
+                      <el-dropdown-item>Action 3</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </template>
+                </el-dropdown>
+              </el-link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    <div class="item">
-      <div
-        :class="{
-          'music-item1': index % 2 !== 1,
-          'music-item2': index % 2 !== 0
-        }"
-        :style="{
-          'border-top-left-radius': index === 0 ? '1rem' : '0',
-          'border-top-right-radius': index === 0 ? '1rem' : '0',
-          'border-bottom-left-radius':
-            index === albumInfo.musicList.length - 1 ? '1rem' : '0',
-          'border-bottom-right-radius':
-            index === albumInfo.musicList.length - 1 ? '1rem' : '0'
-        }"
-        v-for="(item, index) in albumInfo.musicList"
-        :key="index"
-      >
-        <div
-          @click="toMusicInfo(item.id)"
-          class="h-full flex items-center cursor-pointer"
-        >
-          <span class="music index">{{ index + 1 }}</span>
-          <span class="music">
-            {{ item.musicName }}
-          </span>
-        </div>
-        <div class="h-full grow cursor-pointer" @click="toMusicInfo(item.id)" />
-        <div>
-          <span class="mr-4">{{ dateFormater("mm:ss", item.timeLength) }}</span>
-          <el-link :underline="false" class="mr-8">
-            <el-dropdown>
-              <el-icon :size="20" class="cursor-pointer">
-                <Icon icon="ep:more-filled" />
-              </el-icon>
-              <template #dropdown>
-                <el-dropdown-menu>
-                  <el-dropdown-item>Action 1</el-dropdown-item>
-                  <el-dropdown-item>Action 2</el-dropdown-item>
-                  <el-dropdown-item>Action 3</el-dropdown-item>
-                </el-dropdown-menu>
-              </template>
-            </el-dropdown>
-          </el-link>
-        </div>
-      </div>
-    </div>
+      </template>
+    </el-skeleton>
   </div>
 </template>
 <style lang="scss" scoped>
 @import "@/style/element/dialog.scss";
+
+.layout-container {
+  display: flex;
+  flex-direction: row;
+  gap: 3rem;
+
+  @media screen and (max-width: 1024px) {
+    flex-direction: column;
+  }
+}
 
 .info {
   width: 20rem;
@@ -273,7 +346,6 @@ const toArtist = id => {
 }
 
 .show-artist-data {
-  margin-left: 2rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
