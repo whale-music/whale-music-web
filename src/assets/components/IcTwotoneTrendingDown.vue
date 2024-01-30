d1c5769dc2646467ccfe1e4a91f323eaeef32d31
asddfdf <468382010@qq.com>
2024-01-30 15:07:15 +0800

    feat: 添加icon

---
+++ src/assets/components/IcTwotoneTrendingDown.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="m16 18l2.29-2.29l-4.88-4.88l-4 4L2 7.41L3.41 6l6 6l4-4l6.3 6.29L22 12v6z"
+    ></path>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: "IcTwotoneTrendingDown"
+};
+</script>
---
+++ src/assets/components/IcTwotoneTrendingFlat.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path fill="currentColor" d="m22 12l-4-4v3H3v2h15v3z" />
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: "IcTwotoneTrendingFlat"
+};
+</script>
