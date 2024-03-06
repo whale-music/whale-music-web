106f9e8ee74e8c4e3c77790ec2037f537a7cfe3a
asddfdf <468382010@qq.com>
2024-03-06 18:50:50 +0800

    perf: 删除多余代码

--- src/api/picture.ts
+++
@@ -1,6 +0,0 @@
-import type { R } from "@/api/model/common";
-import { http } from "@/utils/http";
-
-export const deleteSourceMusic = (file: string) => {
-  return http.request<R<string>>("get", `/admin/pic/get/temp/${file}`);
-};
