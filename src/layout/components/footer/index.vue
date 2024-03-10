33aed0b4867c4e696e19ce23dc1968f01d2276dd
asddfdf <468382010@qq.com>
2024-03-10 14:48:44 +0800

    feat: 修改footer

--- src/layout/components/footer/index.vue
+++ src/layout/components/footer/index.vue
@@ -8,20 +8,24 @@ const TITLE = getConfig("Title");
   <footer
     class="layout-footer text-[rgba(0,0,0,0.6)] dark:text-[rgba(220,220,242,0.8)]"
   >
-    Copyright © 2020-present
-    <a
-      class="hover:text-primary"
-      href="https://github.com/pure-admin"
-      target="_blank"
-    >
-      &nbsp;{{ TITLE }}
-    </a>
+    <div>Powered by pure-admin</div>
+    <div>
+      Copyright © 2022-present
+      <a
+        class="hover:text-primary"
+        href="https://github.com/whale-music"
+        target="_blank"
+      >
+        &nbsp;{{ TITLE }}
+      </a>
+    </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
 .layout-footer {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
